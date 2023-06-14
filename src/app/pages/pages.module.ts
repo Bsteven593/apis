c2954449c4cc47ac14ebd8e9f7cea6853133f23a
Bsteven593 <bsm.latacumba@yavirac.edu.ec>
2023-06-13 22:11:25 -0500

    apis

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { TareaComponent } from './tarea/tarea.component';
+import { AlumnoComponent } from './alumno/alumno.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    CategoryComponent,
+    ProductComponent,
+    PagesComponent,
+    TareaComponent,
+    AlumnoComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  exports: [
+    DashboardComponent,
+    CategoryComponent,
+    ProductComponent,
+    PagesComponent
+  ],
+})
+export class PagesModule { }
